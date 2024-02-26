f1ef43edbc6f5e7e4304f3a5a4341c8c8cdaf730
DrDeLaBill <40359652+DrDeLaBill@users.noreply.github.com>
2024-02-26 16:46:28 +0400

    Fix release

---
+++ Modules/ModbusManager/modbus_settings.h
@@ -0,0 +1,24 @@
+/* Copyright © 2024 Georgy E. All rights reserved. */
+
+#ifndef _MODBUS_RTU_SETTINGS_H_
+#define _MODBUS_RTU_SETTINGS_H_
+
+
+/*************************** MODBUS REGISTER SETTINGS BEGIN ***************************/
+
+/* Slave registers count */
+#define MODBUS_SLAVE_INPUT_COILS_COUNT                  (0)    // MODBUS default: 9999
+#define MODBUS_SLAVE_OUTPUT_COILS_COUNT                 (0)    // MODBUS default: 9999
+#define MODBUS_SLAVE_INPUT_REGISTERS_COUNT              (16)    // MODBUS default: 9999
+#define MODBUS_SLAVE_OUTPUT_HOLDING_REGISTERS_COUNT     (170)    // MODBUS default: 9999
+
+/* Expected registers count (master) */
+#define MODBUS_MASTER_INPUT_COILS_COUNT                 (0)    // MODBUS default: 9999
+#define MODBUS_MASTER_OUTPUT_COILS_COUNT                (0)    // MODBUS default: 9999
+#define MODBUS_MASTER_INPUT_REGISTERS_COUNT             (0)    // MODBUS default: 9999
+#define MODBUS_MASTER_OUTPUT_HOLDING_REGISTERS_COUNT    (0)    // MODBUS default: 9999
+
+/**************************** MODBUS REGISTER SETTINGS END ****************************/
+
+
+#endif
